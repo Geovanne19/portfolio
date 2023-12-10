4c3a67728edbb751a47b9ea149bbedc13e0088f7
Geovanne19 <130259323+Geovanne19@users.noreply.github.com>
2023-12-10 17:30:19 -0300

    .

--- script.js
+++ script.js
@@ -20,3 +20,27 @@ function alternarTema() {
     var root = document.documentElement;
     root.classList.toggle('tema-claro');
 }
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY
+        let offset = sec.offsetTop - 100
+        let height = sec.offsetHeight
+        let id = sec.getAttribute('id')
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('ativo')
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('ativo')
+            })
+        }
+    })
+
+    let header = document.querySelector('header')
+    header.classList.toggle('sticky', window.scrollY > 100)
+}
