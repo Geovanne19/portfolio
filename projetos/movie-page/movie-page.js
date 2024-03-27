a0b35b99e2daee41408425a68503ebbcbc341f2b
Geovanne19 <130259323+Geovanne19@users.noreply.github.com>
2024-03-26 21:16:27 -0300

    .

--- projetos/movie-page/movie-page.js
+++ projetos/movie-page/movie-page.js
@@ -1,33 +1,11 @@
-let lastScrollTop = 0;
-
-window.addEventlistaener("scroll", function() {
-    let currentScroll = window.scrollY || document.documentElement.scrollTop;
-    
-    if (currentScroll > lastScrollTop) {
-        // Scroll para baixo
-        document.getElementById("header").style.top = "-70px"; // Oculta o header
-    } else {
-        // Scroll para cima
-        document.getElementById("header").style.top = "0"; // Mostra o header
-    }
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Para lidar com scroll no topo da página
-
-    if(currentScroll != 0) {
-        document.getElementById("header").style.background = "#0B141B";
-    } else {
-        document.getElementById("header").style.background = "transparent";
-    }
-}, false);
-
-//step 1: get DOM
 let nextDom = document.getElementById('next');
 let prevDom = document.getElementById('prev');
 
-let carrosselDom = document.querySelector('.carrossel');
-let SliderDom = carrosselDom.querySelector('.carrossel .lista');
-let thumbnailBorderDom = document.querySelector('.carrossel .thumbnail');
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
+let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
 let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
-let timeDom = document.querySelector('.carrossel .time');
+let timeDom = document.querySelector('.carousel .time');
 
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
 let timeRunning = 3000;
@@ -44,23 +22,23 @@ let runTimeOut;
 let runNextAuto = setTimeout(() => {
     next.click();
 }, timeAutoNext)
-function showSlider(tipo) {
-    let SliderItemsDom = SliderDom.querySelectorAll('.carrossel .lista .item');
-    let thumbnailItemsDom = document.querySelectorAll('.carrossel .thumbnail .item');
+function showSlider(type) {
+    let SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
 
-    if (tipo === 'next') {
+    if (type === 'next') {
         SliderDom.appendChild(SliderItemsDom[0]);
         thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-        carrosselDom.classlista.add('next');
+        carouselDom.classList.add('next');
     } else {
         SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
         thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
-        carrosselDom.classlista.add('prev');
+        carouselDom.classList.add('prev');
     }
     clearTimeout(runTimeOut);
     runTimeOut = setTimeout(() => {
-        carrosselDom.classlista.remove('next');
-        carrosselDom.classlista.remove('prev');
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
     }, timeRunning);
 
     clearTimeout(runNextAuto);
@@ -68,3 +46,25 @@ function showSlider(tipo) {
         next.click();
     }, timeAutoNext)
 }
+
+//step 1: get DOM
+let lastScrollTop = 0;
+
+window.addEventlistaener("scroll", function () {
+    let currentScroll = window.scrollY || document.documentElement.scrollTop;
+
+    if (currentScroll > lastScrollTop) {
+        // Scroll para baixo
+        document.getElementById("header").style.top = "-70px"; // Oculta o header
+    } else {
+        // Scroll para cima
+        document.getElementById("header").style.top = "0"; // Mostra o header
+    }
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Para lidar com scroll no topo da página
+
+    if (currentScroll != 0) {
+        document.getElementById("header").style.background = "#0B141B";
+    } else {
+        document.getElementById("header").style.background = "transparent";
+    }
+}, false);
