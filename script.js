67f625a0d53a6dd12aef2ed0c90ebe9aa8dc8d46
Geovanne19 <130259323+Geovanne19@users.noreply.github.com>
2024-08-23 21:56:15 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
+// Função que adiciona ou remove as classes
+function toggleProjetoClasses(projeto) {
+    const verMaisProjeto = projeto.querySelector('.verMais-projeto');
+    const data = verMaisProjeto.querySelector('p');
+    const btnVerMais = verMaisProjeto.querySelector('.btn-verMais');
+
+    // Toggle das classes
+    verMaisProjeto.classList.toggle('extended');
+    data.classList.toggle('visible');
+    btnVerMais.classList.toggle('visible');
+}
+
+// Seleciona todos os elementos com a classe 'projeto'
+const projetos = document.querySelectorAll('.projeto');
+
+// Adiciona os eventos de 'mouseenter', 'mouseleave' e 'click' para cada elemento
+projetos.forEach(function (projeto) {
+    // Para desktop: 'mouseenter' e 'mouseleave'
+    projeto.addEventListener('mouseenter', function () {
+        toggleProjetoClasses(projeto);
+    });
+
+    projeto.addEventListener('mouseleave', function () {
+        toggleProjetoClasses(projeto);
+    });
+
+    // Para dispositivos móveis: 'click'
+    projeto.addEventListener('click', function () {
+        toggleProjetoClasses(projeto);
+    });
+});
+
 let menuIcon = document.querySelector('#menu-icon')
 let navbar = document.querySelector('.navbar')
-
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x')
