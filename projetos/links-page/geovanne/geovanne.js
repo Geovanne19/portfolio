6e617331b7fc457851d8127064cf64a88554febb
Geovanne19 <130259323+Geovanne19@users.noreply.github.com>
2024-08-25 12:18:03 -0300

    .

--- projetos/links-page/geovanne/geovanne.js
+++ projetos/links-page/geovanne/geovanne.js
@@ -1,3 +1,23 @@
-const ancoras = document.querySelectorAll('a')
+function checkAncoras() {
+    const anchors = document.querySelectorAll('a');
 
-ancoras.add
+    // Itera sobre cada âncora
+    anchors.forEach(anchor => {
+        // Adiciona um ouvinte de evento de clique a cada âncora
+        anchor.addEventListener('click', function (event) {
+            if (anchor.getAttribute('href') === '#') {
+                const erro = document.getElementById('mensagem-erro')
+                erro.classList.add('show')
+
+                // Previne o comportamento padrão do clique
+                event.preventDefault();
+
+                setTimeout(() => {
+                    erro.classList.remove('show')
+                }, 2000)
+            }
+        });
+    });
+}
+
+checkAncoras();
